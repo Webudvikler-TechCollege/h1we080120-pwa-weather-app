70c2c34a6cd3e93da9ba37116979653b2fb42f0d
Heinz K <heka@techcollege.dk>
2021-03-08 11:05:59 +0100

    INit Commit

---
+++ src/api/fetchWeather.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "4d58d6f0a435bf7c5a52e2030f17682d";
+
+export const fetchWeather = async (query) => {
+    const params = {
+        q: query,
+        APPID: API_KEY,
+        units: 'metric'
+    }
+
+    const { data } = await axios.get(URL, {
+        params: params
+    })
+
+    return data;
+}
