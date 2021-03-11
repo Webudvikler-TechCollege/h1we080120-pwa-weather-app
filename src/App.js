cf08f7ebd2dcf075e260e3a9ac8c3b36c0ad9b88
Heinz K <heka@techcollege.dk>
2021-03-11 13:41:12 +0100

    Route Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Forecast } from './pages/Forecast'
 import { Start } from './pages/Start'
 
 function App() {
@@ -8,14 +9,24 @@ function App() {
     <div className="main-container">
       <header>
         <h1>PWA Weather App</h1>
+        <ul>
+          <li><a href="/start">Forside</a></li>
+          <li><a href="/forecast">5 døgns prognose</a></li>
+        </ul>
       </header>
       <Router>
         <Switch>
           <Route exact path="/start">
             <Start />
           </Route>
+          <Route exact path="/forecast">
+            <Forecast />
+          </Route>
           <Route exact path="/">
             <Redirect to="/start" />
+          </Route>
+          <Route path="/">
+            <h2>404: Siden fandtes ikke.</h2>
           </Route>
         </Switch>
       </Router>
